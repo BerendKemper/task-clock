09dd46e824693962e58c4632fbf3e2a9d8a48f0a
Berend Kemper <berend_kemper@hotmail.com>
2021-03-12 16:15:20 +0100

    internalTaskClock vs private proto methods

--- index.js
+++ index.js
@@ -3,44 +3,35 @@ const sMs = 1000;
 const mMs = sMs * 60;
 const hMs = mMs * 60;
 const dMs = hMs * 24;
-function PrivateTaskClock(options, parent) {
-	if (options.ticks > Number.MAX_SAFE_INTEGER)
-		throw Error("Ticks should not go beyond safe integer");
-	this.parent = parent;
-	this.taskTime = options.start || new Date(Date.now() - 1);
-	const { interval: { d = 0, h = 0, m = 0, s = 0, ms = 0 } = {} } = options;
-	this.intervalMs = d * dMs + h * hMs + m * mMs + s * sMs + ms || sMs;
-	this.ticks = options.ticks || Infinity;
-	this.tick = 0;
-	this.done = false;
-};
-PrivateTaskClock.prototype = {
-	fireTask() {
-		this.parent.task(new this.parent.DateModel(), ++this.tick);
-		if (this.tick === this.ticks)
-			this.parent.finish();
-	},
-	nextTick() {
+class TaskClock {
+	#taskTime;
+	#intervalMs;
+	#ticks = Infinity;
+	#tick = 0;
+	#done = false;
+	#clock;
+	#nextTick() {
 		const nowMs = Date.now();
-		let taskTime = this.taskTime.getTime();
+		let taskTime = this.#taskTime.getTime();
 		let fire = false;
 		if (nowMs >= taskTime) {
-			if (this.done === true)
-				return this.fireTask();
-			taskTime += this.intervalMs;
+			if (this.#done === true) {
+				this.task(new this.DateModel(), ++this.#tick);
+				return this.#clock = undefined;
+			}
+			taskTime += this.#intervalMs;
 			fire = true;
 			if (nowMs > taskTime)
-				taskTime += this.intervalMs * Math.ceil((nowMs - taskTime) / this.intervalMs);
+				taskTime += this.#intervalMs * Math.ceil((nowMs - taskTime) / this.#intervalMs);
 		}
-		this.clock = setTimeout(() => this.nextTick(), (taskTime - Date.now()) * 0.95);
+		this.#clock = setTimeout(() => this.#nextTick(), (taskTime - Date.now()) * 0.95);
 		if (fire) {
-			this.taskTime.setTime(taskTime);
-			this.fireTask();
+			this.#taskTime.setTime(taskTime);
+			this.task(new this.DateModel(), ++this.#tick);
+			if (this.#tick === this.#ticks)
+				this.finish();
 		}
-	}
-};
-class TaskClock {
-	#private;
+	};
 	/**
 	 * Developers can configure their desired TaskClock. the methods task and lastTick
 	 * can be overwritten as options during construction or extend antoher class from 
@@ -57,14 +48,26 @@ class TaskClock {
 	 * @param {Number} options.ticks
 	 * @param {Function} options.task
 	 * @param {Function} options.lastTick
+	 * @param {Function} options.lastTick
 	 **/
 	constructor(options = {}) {
 		if (typeof options.task === "function")
-			this.lastTick = options.task;
+			this.task = options.task;
 		if (typeof options.lastTick === "function")
 			this.lastTick = options.lastTick;
-		this.#private = new PrivateTaskClock(options, this);
-		this.#private.nextTick();
+		this.#taskTime = options.start instanceof Date ? options.start : new Date(Date.now() - 1);
+		const { interval: { d = 0, h = 0, m = 0, s = 0, ms = 0 } = {} } = options;
+		this.#intervalMs = d * dMs + h * hMs + m * mMs + s * sMs + ms || sMs;
+		this.#ticks = options.ticks < Number.MAX_SAFE_INTEGER ? options.ticks : Infinity;
+		if (options.autoStart !== false)
+			this.#nextTick();
+	};
+	start() {
+		if (!this.#clock) {
+			this.#tick = 0;
+			this.#done = false;
+			this.#nextTick();
+		}
 	};
 	task(now, tick) {
 		console.log(now.toISOString(), "running task", tick);
@@ -74,28 +77,29 @@ class TaskClock {
 	};
 	finish() {
 		this.task = this.lastTick;
-		this.#private.done = true;
+		this.#done = true;
 	};
 	stop() {
-		clearTimeout(this.#private.clock);
-		this.#private.done = true;
-		this.lastTick(new this.DateModel(), this.tick);
+		clearTimeout(this.#clock);
+		this.#done = true;
+		this.#clock = undefined;
+		this.lastTick(new this.DateModel(), this.#tick);
 	};
 	get ticks() {
-		return this.#private.ticks;
+		return this.#ticks;
 	};
 	get tick() {
-		return this.#private.tick;
+		return this.#tick;
 	};
 	get intervalMs() {
-		return this.#private.intervalMs;
+		return this.#intervalMs;
 	};
 	get nextTick() {
-		return this.#private.taskTime.getTime();
+		return this.#taskTime.getTime();
 	};
 	get done() {
-		return this.#private.done;
-	}
+		return this.#done;
+	};
 	get DateModel() {
 		return Date;
 	};
