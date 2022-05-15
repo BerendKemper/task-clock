8ca75f961f8143b86de9dd78d73a363b1974ff0f
Berend Kemper <berend_kemper@hotmail.com>
2022-05-15 14:54:48 +0200

    task-clock@3.0.0 +export as module +d.ts

---
+++ index.d.ts
@@ -0,0 +1,87 @@
+module "task-clock" {
+    declare class TaskClock {
+        /**Configure a taskClock to schedule functions.
+         * ```javascript
+         * new TaskClock(options?: {
+         *     start?: Date, // Default now
+         *     autoStart?: boolean, // Default true
+         *     interval?: { d?: number, h?: number, m?: number, s?: number, ms?: number }, // Default 1000 ms
+         *     ticks?: number, // Default Infinity
+         *     task?: function onTick(now: Date, tick: number) {},
+         *     lastTick?: function onTick(now: Date, tick: number) {}
+         * })
+         * ```
+         * - If the option `start` is set to a date in the past the taskClock invokes the `task` immediately.
+         * - If the option `autoStart` is set to `false` the taskClock does not start untill the method `start` is invoked.
+         *
+         * Example:
+         * ```javascript
+         * new TaskClock({ start: new Date(new Date().setHours(0, 0, 0, 0)), interval: { h: 2 } });
+         * //2022-05-07T23:21:57.397Z running task 1
+         * //2022-05-08T00:00:00.001Z running task 2
+         * //2022-05-08T02:00:00.000Z running task 3
+         * ```
+         **/
+        constructor(options: TaskClockOptions);
+        /**
+         * In case `autoStart` was set to `false` invoke this method manually.
+         */
+        start(): void;
+        /**
+         * This is the default `task` method. A class extended from TaskClock can be created with a `task` method. In case the `task` option was given in the constructor it becomes an instance function. In case the `task` property is overriden from a taskClock instance it becomes the `task`.
+         */
+        task(now: Date, tick: number): void;
+        /**
+         * This is the default `lastTick` method. A class extended from TaskClock can be created with a `lastTick` method. In case the `lastTick` option was given in the constructor it becomes an instance function. In case the `lastTick` property is overriden from a taskClock instance it becomes the `lastTick`.
+         */
+        lastTick(now: Date, tick: number): void;
+        /**
+         * Schedule the next tick to be the `lastTick` and then stops the taskClock.
+         */
+        finish(): void;
+        /**
+         * Immediately stops the taskClock and invokes the `lastTick`.
+         */
+        stop(): void;
+        /**
+         * The amount of ticks untill `lastTick`.
+         */
+        ticks: number;
+        /**
+         * The current tick.
+         */
+        tick: number;
+        /**
+         * The interval in milliseconds.
+         */
+        intervalMs: number;
+        /**
+         * Timestamp in milliseconds of the next tick.
+         */
+        nextTick: number;
+        /**
+         * Indication there are no more active timeouts.
+         */
+        done: boolean;
+        /**
+         * Overrides the `now` parameter that is passed over to the `task` and `lastTick` functions. `DateModel` can only be set by a value that is extended from `Date`.
+         */
+        DateModel: typeof Date
+    }
+}
+interface TaskClockOptions {
+    start?: Date;
+    autoStart?: boolean;
+    interval?: Interval;
+    ticks?: number;
+    task?: onTick;
+    lastTick?: onTick;
+}
+interface Interval {
+    d?: number;
+    h?: number;
+    m?: number;
+    s?: number;
+    ms?: number;
+}
+type onTick = (now: Date, tick: number) => void;
--- index.js
+++ index.js
@@ -4,22 +4,21 @@ const mMs = sMs * 60;
 const hMs = mMs * 60;
 const dMs = hMs * 24;
 let DateModel = Date;
-/**@callback tick @param {Date} now @param {number} tick*/
-class TaskClock {
-    #taskTime;
-    #intervalMs;
+module.exports.TaskClock = class TaskClock {
+    #taskTime = null;
+    #intervalMs = null;
     #ticks = Infinity;
     #tick = 0;
-    #done = false;
-    #clock;
+    #end = false;
+    #clock = null;
     #nextTick() {
         const nowMs = Date.now();
         let taskTime = this.#taskTime.getTime();
         let fire = false;
         if (nowMs >= taskTime) {
-            if (this.#done === true) {
-                this.task(new this.DateModel(), ++this.#tick);
-                return this.#clock = undefined;
+            if (this.#end === true) {
+                this.lastTick(new this.DateModel(), ++this.#tick);
+                return this.#clock = null;
             }
             taskTime += this.#intervalMs;
             fire = true;
@@ -29,16 +28,10 @@ class TaskClock {
         this.#clock = setTimeout(() => this.#nextTick(), (taskTime - Date.now()) * 0.95);
         if (fire) {
             this.#taskTime.setTime(taskTime);
-            this.task(new this.DateModel(), ++this.#tick);
-            if (this.#tick === this.#ticks) {
-                this.task = this.lastTick;
-                this.#done = true;
-            }
+            this.#end = ++this.#tick === this.#ticks;
+            this.task(new this.DateModel(), this.#tick);
         }
-    };
-    /**Developers can configure their desired TaskClock. the methods task and lastTick can be overwritten as options during construction or extend antoher class from TaskClock and give it the prototype methods task and lastTick. Configure the option ticks to finish TaskClock ater given ticks that triggers lastTick.
-     * @param {String} name
-     * @param {{start:Date autoStart:boolean interval:{h:number m:number s:number ms:number} ticks:number task:tick lastTick:tick}} options**/
+    }
     constructor(options = {}) {
         if (typeof options.task === "function")
             this.task = options.task;
@@ -47,56 +40,56 @@ class TaskClock {
         this.#taskTime = options.start instanceof Date ? options.start : new Date(Date.now() - 1);
         const { interval: { d = 0, h = 0, m = 0, s = 0, ms = 0 } = {} } = options;
         this.#intervalMs = d * dMs + h * hMs + m * mMs + s * sMs + ms || sMs;
-        // 2147483647 CANNOT BE LONGER THAN 32 bit int
         this.#ticks = options.ticks < Number.MAX_SAFE_INTEGER ? options.ticks : Infinity;
         if (options.autoStart !== false)
             this.#nextTick();
-    };
+    }
     start() {
         if (!this.#clock) {
             this.#tick = 0;
-            this.#done = false;
+            this.#end = false;
             this.#nextTick();
         }
-    };
-    /**@param {Date} now @param {number} tick*/
+    }
     task(now, tick) {
         console.log(now.toISOString(), "running task", tick);
-    };
-    /**@param {Date} now @param {number} tick*/
+    }
     lastTick(now) {
         console.log(now.toISOString(), "done");
-    };
+    }
     finish() {
         this.task = this.lastTick;
-        this.#done = true;
-    };
+        this.#end = true;
+    }
     stop() {
         clearTimeout(this.#clock);
-        this.#done = true;
-        this.#clock = undefined;
+        this.#end = true;
+        this.#clock = null;
         this.lastTick(new this.DateModel(), this.#tick);
-    };
+    }
     get ticks() {
         return this.#ticks;
-    };
+    }
     get tick() {
         return this.#tick;
-    };
+    }
     get intervalMs() {
         return this.#intervalMs;
-    };
+    }
     get nextTick() {
         return this.#taskTime.getTime();
-    };
+    }
     get done() {
-        return this.#done;
-    };
+        return this.#clock === null;
+    }
     get DateModel() {
         return DateModel;
-    };
+    }
     set DateModel(OwnDate) {
         DateModel = OwnDate;
-    };
+    }
 };
-module.exports = TaskClock;
+// Number.MAX_SAFE_INTEGER === 9007199254740991
+// let maxDate =      new Date(8640000000000000); // Sat Sep 13 275760 02:00:00 GMT+0200 (Central European Summer Time)
+// Number.MIN_SAFE_INTEGER === -9007199254740991
+// let minDate =      new Date(-8640000000000000); // Tue Apr 20 -271821 00:19:32 GMT+0019 (Central European Summer Time)
